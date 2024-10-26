96541d56e2e1296d48fbf557e6d476732a54f1d5
karmael161 <ekarmael@gmail.com>
2024-10-26 13:46:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '237694722989'
+global.nomerowner = ["237694722989"]
 
 // Apikey 
 
